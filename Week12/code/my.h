96fadb4c6bdbac4cc3ca41188a9cede2e3275a13
someday-seven <354774213@qq.com>
2019-11-29 16:30:47 +0800

    Week12

---
+++ Week12/code/my.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<errno.h>
+#include<string.h>
+#include<fcntl.h>
+#include<signal.h>
+#include<time.h>
